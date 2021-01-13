df0d3130dbad9629cce4f0555807ec243999ded5
Evan Furniss <evanfurniss@gmail.com>
2021-01-13 17:34:19 -0600

    Created click event to contact weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,16 @@ function onLoad() {
     let searchBar = $("<input>").attr("placeholder", "City Name");
     $("#searchCity").append(searchBar, searchBtn);
 }
+
+$("#searchBtn").on("click", function(e){
+    e.preventDefault();
+    let city = $("input").val().trim();
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f53d3bed696d22da625060991087f2e7`;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    });
+});
