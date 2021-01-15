958a796e7b83403a15fe52b4955ddf10ab3b603e
Evan Furniss <evanfurniss@gmail.com>
2021-01-14 23:55:51 -0600

    Returning tomorrow to add more

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# weather-app
-Weather App
+# Weather App
+
+## Overview
+
+This app was created to allow users to search for a location via city name. Upon clicking search, the user will be given the current weather status, including temperature, humidity, wind speed, and UV index. The user is also given a five day forecast for the location which include temperature and humidity. When the user searches for a city, a list of recently searched items will appear below the search bar, which the user may click to pull the temperature information for previously searched results.
+
+Future commits will include current date, local time display, weather icons to match the location's current weather, display cards for the forecast, and dynamically changing UV index color.
+
+When the webpage opens, the base webpage has nothing except the header, search bar, and the word Forecast. JQuery was used to dynamically create new html elements, add text value, and apply attributes. These attributes utilize bootstrap to help format my page. using JQuery we are able to use our personal api keys to search using the ajax function. localStorage was also used to retain search history and display it.
+
+## Technologies utilized
+
+Used the empty() function to remove any html or text content for a certain unit, in this case being targeted using JQuery to select a specific id.
+
+w3schools, bootstrap, dayjs, JQuery were all used.
+
+## Images
+
+![Blank page](assets/images/img1.png)
+![Search page](assets/images/img2.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,48 @@
+let pastCities = $("#pastResults");
+let cityArr = [];
+let uv;
+
 onLoad();
 
 function onLoad() {
-    // let headBanner = $("<h1>").text("Weather App").attr("class", "notification center");
-    $("#headerApp").text("Weather App").attr("class", "center title");
-
-    let searchBtn = $("<button>").text("Search").attr({
-        id: "searchBtn"
-    });
-    let searchBar = $("<input>").attr({
-        placeholder: "City Name"
-    });
-    $("#searchCity").append(searchBar, searchBtn);
+    let returnCities = JSON.parse(localStorage.getItem("cities"));
+    if (returnCities !== null) {
+        cityArr = returnCities;
+    }
+    displayHistory();
 }
 
-$("#searchBtn").on("click", function(e){
+function setPastSearches(city) {
+    localStorage.setItem("cities", JSON.stringify(city));
+}
+
+function displayHistory() {
+    pastCities.empty();
+
+    for (let i = 0; i < cityArr.length; i++) {
+        let li = $("<li>").text(cityArr[i]).attr("class", "list-group-item thatsHistory");
+        pastCities.prepend(li);
+    }
+}
+
+$("#searchCity").on("click", function(e){
     e.preventDefault();
     let city = $("input").val().trim();
+    cityArr.push(city);
+    setPastSearches(cityArr);
+    todaysWeather(city);
+    displayHistory();
+});
+
+function todaysWeather(city) {
+    $("#displayResults").empty();
+
     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f53d3bed696d22da625060991087f2e7`;
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        console.log(response);
         let temp = ((response.main.temp - 273.15) * 1.80 + 32).toFixed(2);
         let cityName = response.name;
         let cityTag = $("<h1>").text(cityName).attr("class", "title");
@@ -31,41 +51,48 @@ $("#searchBtn").on("click", function(e){
         let windSpeed = $("<p>").text(`Wind speed: ${response.wind.speed}`);
         let container = $("<div>").append(currTemp, humidity, windSpeed).attr("class", "subtitle notification");
         let br = $("<br>");
-        let uv = $("<p>").text(uvIndex(`UV Index: ${response.coord.lat, response.coord.lon}`));
-        console.log(uv);
+        uvIndex(response.coord.lat, response.coord.lon);
 
         fiveDayForecast(cityName);
         $("#displayResults").append(cityTag, br, container);
     });
-});
+}
 
 function fiveDayForecast(city) {
+    $("#fiveDay").empty();
+
     let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=f53d3bed696d22da625060991087f2e7`;
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        console.log(response);
         let fiveDayArr =[];
+        let j = 1;
         for (let i = 3; i < 40; i+=8) {
             let dayCreator = {
+                day: $("<h4>").text(`Day ${j}`),
                 temp: $("<p>").text(`Temperature: ${((response.list[i].main.temp - 273.15) * 1.80 + 32).toFixed(2)}`),
                 humidity: $("<p>").text(`Humidity: ${response.list[i].main.humidity}`)
             }
+            j++;
             fiveDayArr.push(dayCreator);
         }
-        $("#fiveDay").append(fiveDayArr[0].temp, fiveDayArr[0].humidity, fiveDayArr[1].temp, fiveDayArr[1].humidity, fiveDayArr[2].temp, fiveDayArr[2].humidity, fiveDayArr[3].temp, fiveDayArr[3].humidity, fiveDayArr[4].temp, fiveDayArr[4].humidity,);
+        $("#fiveDay").append(fiveDayArr[0].day, fiveDayArr[0].temp, fiveDayArr[0].humidity, fiveDayArr[1].day, fiveDayArr[1].temp, fiveDayArr[1].humidity, fiveDayArr[2].day, fiveDayArr[2].temp, fiveDayArr[2].humidity, fiveDayArr[3].day, fiveDayArr[3].temp, fiveDayArr[3].humidity, fiveDayArr[4].day, fiveDayArr[4].temp, fiveDayArr[4].humidity,);
     });
 };
 
 function uvIndex (lat, lon) {
-    let queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily,alerts&appid=f53d3bed696d22da625360991087f2e7`
-    let uv = $.ajax({
+    let queryURL = `https://api.openweathermap.org/data/2.5/uvi?appid=f53d3bed696d22da625060991087f2e7&lat=${lat}&lon=${lon}`
+    $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        return response.current.uvi;
-    });
-    uvCreator = $("<p>").text(`UV Index: ${uv}`)
-    return uvCreator;
+        uv = $("<p>").text(`UV Index: ${response.value}`);
+        $("#displayResults").append(uv);
+    })
 };
+
+$(".thatsHistory").on("click", function(e){
+    e.preventDefault();
+    todaysWeather(e.currentTarget.outerText);
+})
